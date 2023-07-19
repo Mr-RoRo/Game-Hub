7eb93ec366530b8dfc6b35c0f06d64da01e3d1cc
Danial <fsoocity@gmail.com>
2023-07-19 17:07:01 +0330

    Create Genres Static

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genres {
     id: number;
@@ -6,6 +6,6 @@ export interface Genres {
     image_background: string
   }
 
-const useGenres = () => useData<Genres>("/genres")
+const useGenres = () => ({datas: genres , isLoading: false , error: null})
 
 export default useGenres;
